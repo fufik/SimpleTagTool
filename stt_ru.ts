a06738e5c401a112c5898734c559b9b71bdb8c81
fufik <ivaiva199ivaiva@gmail.com>
2016-03-18 21:45:34 +0000

    Started localiztion

---
+++ stt_ru.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <source>SimpleTagTool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <source>Open File</source>
+        <translation type="unfinished">Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="73"/>
+        <source>Album name</source>
+        <translation type="unfinished">Название альбома</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="87"/>
+        <source>Save</source>
+        <translation type="unfinished">Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="98"/>
+        <source>MP3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="27"/>
+        <source>Open MP3 file</source>
+        <translation type="unfinished">Открыть файл MP3</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="27"/>
+        <source>MP3 files (*.mp3)</source>
+        <translation type="unfinished">MP3 файлы (*.mp3)</translation>
+    </message>
+</context>
+</TS>
