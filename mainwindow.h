32104d00fa5949a8fbb9d7900a596276085b8fd3
fufik <ivaiva1999ivaiva@gmail.com>
2016-03-31 18:26:59 +0000

    test comm

--- mainwindow.h
+++ mainwindow.h
@@ -20,18 +20,14 @@ public:
 
 private slots:
     void on_openfilebut_clicked();
-
     void on_openfiledir_textChanged(const QString &arg1);
-
     void on_savebut_clicked();
-
     void on_typech_currentIndexChanged(int index);
     void clearFields();
     void enableFields(bool opfd=0);
     void disableFields(bool opfd=0);
 
 private:
-    //QString filename;
     Ui::MainWindow *ui;
     TagLib::MPEG::File* pTrack;
     QFileInfo* pFile;
